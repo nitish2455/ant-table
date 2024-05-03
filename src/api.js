1d44b731214db42a7facff023eea018891d4e1ee
nitish2455 <nitish98888@gmail.com>
2024-05-03 11:26:18 +0530

    commit

--- src/api.js
+++ src/api.js
@@ -1,11 +1,17 @@
-
 const API_BASE_URL = 'https://dummyjson.com';
 
 export const fetchPosts = async (skip, limit, tags = '', search = '') => {
   try {
-    const response = await fetch(
-      `${API_BASE_URL}/posts?skip=${skip}&limit=${limit}&tags=${tags}&search=${search}`
-    );
+    let response = {};
+    if(search) {
+       response = await fetch(
+        `${API_BASE_URL}/posts/search?q=${search}&skip=${skip}&limit=${limit}&tags=${tags}&search=${search}`
+      );
+    } else {
+      response = await fetch(
+        `${API_BASE_URL}/posts?skip=${skip}&limit=${limit}&tags=${tags}`
+      );
+    }
     if (!response.ok) {
       throw new Error('Failed to fetch posts');
     }
@@ -16,3 +22,20 @@ export const fetchPosts = async (skip, limit, tags = '', search = '') => {
     throw error;
   }
 };
+// const API_BASE_URL = 'https://dummyjson.com';
+
+// export const fetchPosts = async (skip, limit, tags = '', search = '') => {
+//   try {
+//     const response = await fetch(
+//       `${API_BASE_URL}/posts?skip=${skip}&limit=${limit}&tags=${tags}&search=${search}`
+//     );
+//     if (!response.ok) {
+//       throw new Error('Failed to fetch posts');
+//     }
+//     const data = await response.json();
+//     return data.posts; // Extracting the posts array from the response
+//   } catch (error) {
+//     console.error('Error fetching posts:', error);
+//     throw error;
+//   }
+// };
